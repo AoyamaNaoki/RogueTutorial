4059fd806d5324c9fa8c4bb2bb37859f90a87b91
AoyamaNaoki <45162737+AoyamaNaoki@users.noreply.github.com>
2018-11-28 16:32:13 +0900

    2018/11/28

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -117,7 +117,7 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
             hitWall.DamageWall (wallDamage);
 
             //Set the attack trigger of the player's animation controller in order to play the player's attack animation.
-            animator.SetTrigger ("playerChop");
+            animator.SetTrigger ("PlayerChop");
         }
 
 
@@ -171,7 +171,7 @@ using UnityEngine.SceneManagement;      //Allows us to use SceneManager
         public void LoseFood (int loss)
         {
             //Set the trigger for the player animator to transition to the playerHit animation.
-            animator.SetTrigger ("playerHit");
+            animator.SetTrigger ("PlayerHit");
 
             //Subtract lost food points from the players total.
             food -= loss;
